66e8b51d6f12664ef1f0d680693d8ce4d5ef7535
“[mdogdope]” <m45t3rhest@gmail.com>
2018-01-22 22:20:02 -0800

    Made the hash requirement dynamic

--- getprice.php
+++ getprice.php
@@ -1,16 +1,11 @@
 <?php
-$address = $_REQUEST["address"];
-$data = json_decode(file_get_contents("https://api.cryptonator.com/api/ticker/xmr-btc"));
+$data = json_decode(file_get_contents("https://api.cryptonator.com/api/ticker/btc-xmr"));
 if (!$data->success) {
 return 0;
 }
-$price $data->ticker->price;
-echo $price;
+$price = $data->ticker->price;
 
+$val = $price/100;
 
-
-
-
-
-
+echo $payoutrate = ($val/0.000054)*1.05
 ?>
